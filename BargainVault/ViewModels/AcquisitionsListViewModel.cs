79bac1f2641b2f9586e471b796d4f0053273ba73
khins <kevin@hinshawfam.com>
2026-01-31 11:15:44 -0600

    updates list view search

--- BargainVault/ViewModels/AcquisitionsListViewModel.cs
+++ BargainVault/ViewModels/AcquisitionsListViewModel.cs
@@ -3,7 +3,9 @@ using BargainVault.Domain.Models;
 using BargainVault.Domain.Services;
 using BargainVault.ViewModels.Base;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Threading.Tasks;
+using System.Windows.Data;
 using System.Windows.Input;
 
 namespace BargainVault.ViewModels.Acquisitions
@@ -26,7 +28,51 @@ namespace BargainVault.ViewModels.Acquisitions
         public AcquisitionListDto SelectedAcquisition
         {
             get => _selectedAcquisition;
-            set => SetProperty(ref _selectedAcquisition, value);
+            set
+            {
+                SetProperty(ref _selectedAcquisition, value);
+                OnPropertyChanged(nameof(HasSelection));
+            }
+        }
+        public bool HasSelection => SelectedAcquisition != null;
+
+        private string? _searchText;
+        public string? SearchText
+        {
+            get => _searchText;
+            set
+            {
+                SetProperty(ref _searchText, value);
+                ItemsView?.Refresh();
+            }
+        }
+
+        private ICollectionView? _itemsView;
+        public ICollectionView? ItemsView
+        {
+            get => _itemsView;
+            private set => SetProperty(ref _itemsView, value);
+        }
+
+        private bool FilterItems(object obj)
+        {
+            if (obj is not AcquisitionListDto item)
+                return false;
+
+            if (string.IsNullOrWhiteSpace(SearchText))
+                return true;
+
+            var text = SearchText.ToLower();
+
+            return
+                item.ItemTitle?.ToLower().Contains(text) == true;
+        }
+
+        private ObservableCollection<AcquisitionListDto> _items = new();
+        public ObservableCollection<AcquisitionListDto> Items
+        {
+            get => _items;
+            private set => SetProperty(ref _items, value);
         }
 
         public ICommand RefreshCommand { get; }
@@ -38,6 +84,13 @@ namespace BargainVault.ViewModels.Acquisitions
             Acquisitions.Clear();
             foreach (var acq in results)
                 Acquisitions.Add(acq);
+
+            Items = new ObservableCollection<AcquisitionListDto>(results);
+
+            ItemsView = CollectionViewSource.GetDefaultView(Items);
+            ItemsView.Filter = FilterItems;
+
+            OnPropertyChanged(nameof(ItemsView));
         }
     }
 }
--- BargainVault/Views/AcquisitionsListView.xaml.cs
+++ BargainVault/Views/AcquisitionsListView.xaml.cs
@@ -3,6 +3,7 @@ using BargainVault.ViewModels.Acquisitions;
 using BargainVault.Views.Acquisitions;
 using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace BargainVault.Views.Acquisitions
 {
@@ -49,5 +50,38 @@ namespace BargainVault.Views.Acquisitions
             await vm.LoadAsync();
         }
 
+        private void DataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            EditSelected_Click(sender, e);
+        }
+
+        private async void DeleteSelected_Click(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is not AcquisitionsListViewModel vm ||
+                     vm.SelectedAcquisition == null)
+                return;
+
+            var result = MessageBox.Show(
+                "Are you sure you want to delete this acquisition?",
+                "Confirm Delete",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Warning);
+
+            if (result != MessageBoxResult.Yes)
+                return;
+
+            var service = App.Services.GetRequiredService<IAcquisitionsService>();
+
+            await service.DeleteAcquisitionAsync(
+                vm.SelectedAcquisition.AcqId,
+                enteredBy: Environment.UserName);
+
+            await vm.LoadAsync();
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
